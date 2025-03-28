662f15cc5f2dc42bd3ddf490626dda85b3ffbacb
EnzoHashinokutiXavier <enzokuti.xavier@gmail.com>
2025-03-28 16:13:47 -0400

    refactor: savegame correction

--- dados_game.py
+++ dados_game.py
@@ -1,4 +1,4 @@
-nome = 'Gigi Gay'
+nome = 'vazio'
 
 dia = 1
 dinheiro = 2000
@@ -17,9 +17,9 @@ arquant2 = [0, 0, 0, 0, 0, 0, 0, 0]
 nomes_comida = ['maçã',  'pera',  'banana',  'tomate', 'alface', 'batata']
 preco_comida = [14.00, 25.00, 8.50, 8.00, 7.95, 5.00]
 quantia_comida = [11, 7, 6, 7, 3, 4]
-nomes_bebidas = ['Água_s/gás', 'C.cola_mini', 'C.cola_2l', 'Skol_lata', 'Energético']
-preco_bebidas = [30.00, 30.00, 82.30, 48.00, 62.70]
-quantia_bebidas = [12, 12, 8, 12, 8]
+nomes_bebidas = ['Água_s/gás', 'C.cola_mini', 'C.cola_2l', 'Skol_lata', 'Energético', 'DelValle_cx']
+preco_bebidas = [30.00, 30.00, 82.30, 48.00, 62.70, 50.00]
+quantia_bebidas = [12, 12, 8, 12, 8, 6]
 
 l_precos_comida = [14.00, 25.00, 8.50, 8.00, 7.95, 5.00]
 l_precos_bebidas = [30.00, 30.00, 82.30, 48.00]
